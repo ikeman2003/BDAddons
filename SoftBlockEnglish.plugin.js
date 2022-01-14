ccf6e2b30be3bbb2e8f1cf744c55e00d74354765
ikeman2003 <96934345+ikeman2003@users.noreply.github.com>
2022-01-13 19:09:59 -0500

    Revert back to v1.0.2
    Reverting back to original 1.0.2 because 1.0.3 broke stuff

--- SoftBlockEnglish.plugin.js
+++ SoftBlockEnglish.plugin.js
@@ -28,93 +28,13 @@
 @else@*/
 
 module.exports = (() => {
-    const config = {
-        "main": "index.js",
-        "info": {
-            "name": "SoftBlock",
-            "authors": [{
-                "name": "ikeman2003",
-                "github_username": "ikeman2003"
-            }],
-            "version": "1.0.3",
-            "description": "Disable the messages from people you like enough not to block them but don't wanna see them in specific or all channels.",
-            "github": "https://github.com/ikeman2003/BDAddons",
-            "github_raw": "https://raw.githubusercontent.com/ikeman2003/BDAddons/master/SoftBlockEnglish.plugin.js"
-        },
-        "changelog": {
-            "improved": {
-                "Clarity": "Simplifed translation to make more sense and be more descriptive."
-            }
-        },
-        "defaultConfig": [{
-            "type": "textbox",
-            "id": "blocked",
-            "name": "Blocked",
-            "note": "User ID for the person(s) to be softblocked separated by \",\"",
-            "value": "",
-            "placeholder": "178896511378259968, 416275248153886720"
-        }, {
-            "type": "textbox",
-            "id": "servers",
-            "name": "Impacted servers",
-            "note": "Server ID for the server(s) that will be impacted separated by \",\"",
-            "value": "",
-            "placeholder": "727082219318935562, 700448965661032550"
-        }, {
-            "type": "switch",
-            "id": "useEverywhere",
-            "name": "Activate everywhere",
-            "note": "Overrides the permissions of impacted servers and activates globally.",
-            "value": true
-        }, {
-            "type": "switch",
-            "id": "showDm",
-            "name": "Display DMs",
-            "note": "Leave the user unblocked and keeps messages visible in private DM's.",
-            "value": false
-        }, {
-            "type": "switch",
-            "id": "showGuild",
-            "name": "Display in Servers",
-            "note": "Leave user unblocked and messages visible in servers and channels.",
-            "value": false
-        }, {
-            "type": "switch",
-            "id": "showTyping",
-            "name": "View \"typing\"",
-            "note": "Show when softblocked users are typing.",
-            "value": true
-        }, {
-            "type": "switch",
-            "id": "showReplies",
-            "name": "Show Replies",
-            "note": "Show when a softblocked user replies directly to your message.",
-            "value": false
-        }, {
-            "type": "switch",
-            "id": "showMembers",
-            "name": "Display in the members list",
-            "note": "Leave softblocked users in the member list.",
-            "value": false
-        }]
-    };
-
+    const config = {"main":"index.js","info":{"name":"SoftBlock","authors":[{"name":"ikeman2003","github_username":"ikeman2003"}],"version":"1.0.2","description":"Disable the display of messages from people you like enough not to block them","github":"https://github.com/ikeman2003/BDAddons","github_raw":"https://raw.githubusercontent.com/ikeman2003/BDAddons/master/SoftBlockEnglish.plugin.js"},"changelog":[],"defaultConfig":[{"type":"textbox","id":"blocked","name":"Blocked","note":"Identifiers of the persons to be blocked separated by \",\"","value":"","placeholder":"178896511378259968, 416275248153886720"},{"type":"textbox","id":"servers","name":"Impacted servers","note":"Identifiers of the servers to be impacted separated by \",\"","value":"","placeholder":"727082219318935562, 700448965661032550"},{"type":"switch","id":"useEverywhere","name":"Activate everywhere","note":"Overrides the permissions of impacted servers","value":true},{"type":"switch","id":"showDm","name":"Display DMs","note":"Leave visible messages in private messages","value":false},{"type":"switch","id":"showGuild","name":"Display in servers","note":"Leave messages visible in server rooms","value":false},{"type":"switch","id":"showTyping","name":"View \"typing\"","note":"Let it be seen who is typing","value":true},{"type":"switch","id":"showReplies","name":"Show answers","note":"Leave visible the answers to the messages","value":false},{"type":"switch","id":"showMembers","name":"Display in the members list","note":"Leave visible the blocked people in the list of members","value":false}]};
     return !global.ZeresPluginLibrary ? class {
-        constructor() {
-            this._config = config;
-        }
-        getName() {
-            return config.info.name;
-        }
-        getAuthor() {
-            return config.info.authors.map(a => a.name).join(", ");
-        }
-        getDescription() {
-            return config.info.description;
-        }
-        getVersion() {
-            return config.info.version;
-        }
+        constructor() {this._config = config;}
+        getName() {return config.info.name;}
+        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+        getDescription() {return config.info.description;}
+        getVersion() {return config.info.version;}
         load() {
             BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
                 confirmText: "Download Now",
@@ -132,125 +52,114 @@ module.exports = (() => {
     } : (([Plugin, Api]) => {
         const plugin = (Plugin, Library) => {
 
-            const {
-                Logger,
-                Patcher,
-                DiscordAPI
-            } = Library;
+    const {Logger, Patcher, DiscordAPI} = Library;
 
-            return class SoftBlock extends Plugin {
-                removeTyping(guild, channel, blocked) {
-                    let typingElement = Array.from(document.querySelectorAll("div[class^='typing']"))[0];
-                    if (typingElement) {
-                        let membersSpan = typingElement.children[1];
-                        Array.from(membersSpan.children).filter((child) => child.tagName === "STRONG").forEach((strongElement) => {
-                            let guildMember = guild.members.find((member) => member.name === strongElement.innerText);
-                            if (blocked.includes(guildMember.userId)) {
-                                strongElement.style.display = "none";
-                            }
-                            if (Array.from(membersSpan.children).filter((child) => child.style.display !== "none").length === 0) typingElement.style.display = "none";
-                        })
+    return class SoftBlock extends Plugin {
+        removeTyping(guild, channel, blocked) {
+            let typingElement = Array.from(document.querySelectorAll("div[class^='typing']"))[0];
+            if (typingElement) {
+                let membersSpan = typingElement.children[1];
+                Array.from(membersSpan.children).filter((child) => child.tagName === "STRONG").forEach((strongElement) => {
+                    let guildMember = guild.members.find((member) => member.name === strongElement.innerText);
+                    if (blocked.includes(guildMember.userId)) {
+                        strongElement.style.display = "none";
+                    }
+                    if (Array.from(membersSpan.children).filter((child) => child.style.display !== "none").length === 0) typingElement.style.display = "none";
+                })
+            }
+        }
+
+        removeReplies(guild, blocked) {
+            let replies = Array.from(document.querySelectorAll("div[class^='repliedMessage']"));
+            replies.forEach((reply) => {
+                let presumeSpan = reply.children.length > 3 ? reply.children[2] : reply.children[1];
+                if (presumeSpan.tagName === "SPAN") {
+                    let fullText = presumeSpan.innerText;
+                    let nick = fullText.startsWith("@") ? fullText.substring(1) : fullText;
+                    let dMember = guild.members.find((member) => member.name === nick && blocked.includes(member.userId));
+                    if (dMember) reply.style.display = "none"
+                }
+            })
+        }
+
+        removeMembers(guild, blocked) {
+            let members = document.querySelectorAll("div[class^='member-']");
+            members.forEach((memberElement) => {
+                let nick = memberElement.children[0].children[1].children[0].children[0].children[0].innerText;
+                let guildMember = guild.members.find((member) => member.name === nick || member.nickname === nick);
+                if (blocked.includes(guildMember.userId)) {
+                    let previousSibling = memberElement.previousSibling;
+                    memberElement.style.display = "none";
+                    while (previousSibling.tagName !== 'H2') previousSibling = previousSibling.previousSibling;
+                    // let memberCount = previousSibling.firstChild.innerText.split("").pop();
+                    // if (memberCount === "1" || previousSibling.nextSibling.tagName === "H2") {
+                    //     previousSibling.style.display = "none"
+                    // }
+                    let nextNode = previousSibling.nextSibling ? previousSibling.nextSibling : previousSibling;
+                    let elements = [];
+                    while (nextNode.tagName !== 'H2') {
+                        elements.push(nextNode);
+                        if (nextNode.nextSibling) nextNode = nextNode.nextSibling;
+                        else break
+                    }
+                    if (elements.filter((elem) => elem.style.display !== "none").length === 0) previousSibling.style.display = "none";
+                }
+            })
+        }
+
+        removeTextMessages(channel, blocked) {
+            let blockedMessage = channel.messages.filter((message) => message.author !== null && blocked.includes(message.author.id));
+            blockedMessage.forEach((message) => {
+                let elem = document.getElementById("chat-messages-" + message.id);
+                if (elem) elem.style.display = "none"
+            })
+        }
+
+        onStart() {
+            Logger.log("Started");
+            Patcher.before(Logger, "log", (t, a) => {
+                a[0] = "Patched Message: " + a[0];
+            });
+        }
+
+        onStop() {
+            Logger.log("Stopped");
+            Patcher.unpatchAll();
+        }
+
+        getSettingsPanel() {
+            const panel = this.buildSettingsPanel();
+            return panel.getElement();
+        }
+
+        async observer() {
+            try {
+
+                let blocked = this.settings["blocked"].split(/,\s?/);
+                let servers = this.settings["servers"].split(/,\s?/);
+                let {showDm, showGuild, showTyping, showReplies, showMembers, useEverywhere} = this.settings;
+                let channel = DiscordAPI.currentChannel;
+                let guild = DiscordAPI.currentGuild;
+
+                if (useEverywhere || (guild && servers.includes(guild.id))) {
+                    if (!showTyping) await this.removeTyping(guild, channel, blocked);
+                    if (!showMembers) await this.removeMembers(guild, blocked);
+                    if (!showReplies) await this.removeReplies(guild, blocked);
+
+                    if (channel) {
+                        let canExecuteInDM = !showDm && channel.type === "GUILD_TEXT";
+                        let canExecuteInGuild = !showGuild && channel.type === "GUILD_TEXT";
+                        if (canExecuteInDM || canExecuteInGuild) {
+                            await this.removeTextMessages(channel, blocked)
+                        }
                     }
                 }
-
-                removeReplies(guild, blocked) {
-                    let replies = Array.from(document.querySelectorAll("div[class^='repliedMessage']"));
-                    replies.forEach((reply) => {
-                        let presumeSpan = reply.children.length > 3 ? reply.children[2] : reply.children[1];
-                        if (presumeSpan.tagName === "SPAN") {
-                            let fullText = presumeSpan.innerText;
-                            let nick = fullText.startsWith("@") ? fullText.substring(1) : fullText;
-                            let dMember = guild.members.find((member) => member.name === nick && blocked.includes(member.userId));
-                            if (dMember) reply.style.display = "none"
-                        }
-                    })
-                }
-
-                removeMembers(guild, blocked) {
-                    let members = document.querySelectorAll("div[class^='member-']");
-                    members.forEach((memberElement) => {
-                        let nick = memberElement.children[0].children[1].children[0].children[0].children[0].innerText;
-                        let guildMember = guild.members.find((member) => member.name === nick || member.nickname === nick);
-                        if (blocked.includes(guildMember.userId)) {
-                            let previousSibling = memberElement.previousSibling;
-                            memberElement.style.display = "none";
-                            while (previousSibling.tagName !== 'H2') previousSibling = previousSibling.previousSibling;
-                            // let memberCount = previousSibling.firstChild.innerText.split("").pop();
-                            // if (memberCount === "1" || previousSibling.nextSibling.tagName === "H2") {
-                            //     previousSibling.style.display = "none"
-                            // }
-                            let nextNode = previousSibling.nextSibling ? previousSibling.nextSibling : previousSibling;
-                            let elements = [];
-                            while (nextNode.tagName !== 'H2') {
-                                elements.push(nextNode);
-                                if (nextNode.nextSibling) nextNode = nextNode.nextSibling;
-                                else break
-                            }
-                            if (elements.filter((elem) => elem.style.display !== "none").length === 0) previousSibling.style.display = "none";
-                        }
-                    })
-                }
-
-                removeTextMessages(channel, blocked) {
-                    let blockedMessage = channel.messages.filter((message) => message.author !== null && blocked.includes(message.author.id));
-                    blockedMessage.forEach((message) => {
-                        let elem = document.getElementById("chat-messages-" + message.id);
-                        if (elem) elem.style.display = "none"
-                    })
-                }
-
-                onStart() {
-                    Logger.log("Started");
-                    Patcher.before(Logger, "log", (t, a) => {
-                        a[0] = "Patched Message: " + a[0];
-                    });
-                }
-
-                onStop() {
-                    Logger.log("Stopped");
-                    Patcher.unpatchAll();
-                }
-
-                getSettingsPanel() {
-                    const panel = this.buildSettingsPanel();
-                    return panel.getElement();
-                }
-
-                async observer() {
-                    try {
-
-                        let blocked = this.settings["blocked"].split(/,\s?/);
-                        let servers = this.settings["servers"].split(/,\s?/);
-                        let {
-                            showDm,
-                            showGuild,
-                            showTyping,
-                            showReplies,
-                            showMembers,
-                            useEverywhere
-                        } = this.settings;
-                        let channel = DiscordAPI.currentChannel;
-                        let guild = DiscordAPI.currentGuild;
-
-                        if (useEverywhere || (guild && servers.includes(guild.id))) {
-                            if (!showTyping) await this.removeTyping(guild, channel, blocked);
-                            if (!showMembers) await this.removeMembers(guild, blocked);
-                            if (!showReplies) await this.removeReplies(guild, blocked);
-
-                            if (channel) {
-                                let canExecuteInDM = !showDm && channel.type === "GUILD_TEXT";
-                                let canExecuteInGuild = !showGuild && channel.type === "GUILD_TEXT";
-                                if (canExecuteInDM || canExecuteInGuild) {
-                                    await this.removeTextMessages(channel, blocked)
-                                }
-                            }
-                        }
-                    } catch (e) {
-                        return console.log(e);
-                    }
-                }
-            };
-        };
+            } catch (e) {
+                return console.log(e);
+            }
+        }
+    };
+};
         return plugin(Plugin, Api);
     })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
